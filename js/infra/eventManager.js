7c51b8cc1366086ab8e84a0a018e559c39d3bb40
tavisca-idrisw <idrisw@tavisca.com>
2015-07-25 12:04:33 +0530

    Event manager now works asyncnously

--- js/infra/eventManager.js
+++ js/infra/eventManager.js
@@ -1,82 +1,72 @@
 ﻿'use strict';
 
-window.hotel.eventManager = (function() {
+window.eventManager = new function() {
+
   var _subscribe = {}; //private to EventManager 
 
-  return {
-    subscribe: function(eventName, eventHandler) {
-      
-      if ((typeof eventName !== 'string') || (typeof eventHandler !== 'function')){
-        throw new Error('eventName or callback type is Invalid!!!');
-      }
-  
-      var _eventId = new Date().getTime();
-      var eventInfo = {
-        eventId: _eventId,
-        eventHandler: eventHandler
-      };
-  
-      (_subscribe.eventName = _subscribe.eventName || []).push(eventInfo);
-    },
-  
-    unsubscribe: function(eventName, eventHandler) {
-      var args = arguments;
-      /**
-      * If only the eventName is passed 
-      * then we unsubscribe the all events of that type.
-      */
-  
-      if (args.length === 1 && typeof args[0] === 'string') {
-        _subscribe.eventName = [];
-      }
-  
-      /**
-      * Otherwise, remove that specific instance.
-      */
-  
-      else if (typeof eventHandler === 'function') {
-        var subscribe = _subscribe.eventName;
-  
-        if (subscribe !== undefined) {
-          var positionFound = -1;
-  
-          for (var index = subscribe.length - 1; index >= 0; index--) {
-            
-            if(subscribe[index] === eventHandler) {
-              positionFound = index;
-            }
-          }
-  
-          if (positionFound >= 0) {
-            subscribe.splice(positionFound, 1);
-          }
-        }
-      }
-  
-      return this;
-    },
-  
-    fire: function(eventName, reference, params) {
-      
-      var subscribe = _subscribe.eventName;
-      
-      // TODO make params into an array or look for a better solution.
-      
-      params = params || [];
-  
-      if (subscribe !== undefined) {
-        for (var i = 0; i < subscribe.length; i++) {
-          if (subscribe[i].eventHandler === eventHandler)
-          {
-            setTimeout(function() {
-            // TODO figure the reference part out.
-            // refer eventManager.fire('Results Fetched', this, hotelList);
+  this.subscribe = function(eventName, context, eventHandler) {
 
-              eventHandler.apply(reference, params); 
-            });
+    if ((typeof eventName !== 'string') || (typeof eventHandler !== 'function')){
+      throw new Error('eventName or callback type is Invalid!!!');
+    }
+
+    var _eventId = new Date().getTime();
+    var eventInfo = {
+      eventId: _eventId,
+      context: context,
+      eventHandler: eventHandler
+    };
+
+    (_subscribe.eventName = _subscribe.eventName || []).push(eventInfo);
+  };
+
+  this.unsubscribe = function(eventName, eventHandler) {
+    var args = arguments;
+    /**
+    * If only the eventName is passed 
+    * then we unsubscribe the all events of that type.
+    */
+
+    if (args.length === 1 && typeof args[0] === 'string') {
+      _subscribe.eventName = [];
+    }
+
+    /**
+    * Otherwise, remove that specific instance.
+    */
+
+    else if (typeof eventHandler === 'function') {
+      var subscribe = _subscribe.eventName;
+
+      if (subscribe !== undefined) {
+        var positionFound = -1;
+
+        for (var index = subscribe.length - 1; index >= 0; index--) {
+          
+          if(subscribe[index] === eventHandler) {
+            positionFound = index;
           }
         }
+
+        if (positionFound >= 0) {
+          subscribe.splice(positionFound, 1);
+        }
       }
-    },
+    }
+
+    return this;
   };
-})();
+
+  this.fire = function(eventName, sender, params) {
+
+    if (_subscribe.eventName !== undefined) {
+      for (var i = 0; i < _subscribe.eventName.length; i++) {
+
+        setTimeout(function(context, callback, sender, args){
+          callback.apply(context, [sender, args])
+        }, 0, _subscribe.eventName[i].context, _subscribe.eventName[i].eventHandler, sender, params);
+
+      }
+    }
+  };
+}
